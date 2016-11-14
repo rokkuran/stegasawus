d77cf4f73d102cdd54bc85fa735173d80250b53e
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-14 22:20:18 +0000

    Looking at message embedding capacity.

--- encode_images.py
+++ encode_images.py
@@ -1,6 +1,8 @@
 import os
+import numpy
 
 from stegano import lsb
+import matplotlib.pyplot as plt
 
 
 def get_message(filepath):
@@ -35,9 +37,30 @@ if __name__ == '__main__':
     # path_output = '{}/images/train/encoded/'.format(path)
 
     # validation dataset
-    path_images = '/home/rokkuran/workspace/kaggle/painter_by_numbers/train_2/'
-    path_cropped = '{}/images/validation/cropped/'.format(path)
-    path_output = '{}/images/validation/encoded/'.format(path)
+    # path_images = '/home/rokkuran/workspace/kaggle/painter_by_numbers/train_2/'
+    # path_cropped = '{}/images/validation/cropped/'.format(path)
+    # path_output = '{}/images/validation/encoded/'.format(path)
 
     message = get_message(filepath_message)
-    encode_message(path_cropped, message, path_output)
+    # encode_message(path_cropped, message, path_output)
+
+    path_images = '{}/images/train/cropped/'.format(path)
+    path_output = '{}/images/'.format(path)
+    # filename = '19694.jpg'
+    filename = '16.jpg'
+    # image = io.imread(fname='{}{}'.format(path_images, filename))
+    # io.imsave(path_output + filename, image)
+
+    multipliers = [1, 11]
+    for m in multipliers:
+        filepath_image = '{}{}'.format(path_images, filename)
+        secret = lsb.hide(filepath_image, message * m, auto_convert_rgb=True)
+        secret.save('{}{}_{}.jpg'.format(path_output, filename.split('.')[0], m))
+
+
+    image1 = io.imread(fname='{}{}_{}.jpg'.format(path_output, filename.split('.')[0], 1))
+    image2 = io.imread(fname='{}{}_{}.jpg'.format(path_output, filename.split('.')[0], 11))
+
+    plot_image = numpy.concatenate((image1, image2), axis=1)
+    plt.imshow(plot_image)
+    plt.show()
