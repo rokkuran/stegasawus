af256a00877180f8f2f07e1f9f9e3096b384e126
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-14 17:17:14 +0000

    image feature creation from gaussian pyramid
    images processing of statistical features from gaussian pyramid layers with residuals

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import os
 import numpy
+import pandas
 
 import skimage
 import skimage.io as io
@@ -10,7 +11,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 
 
-def pyramid(image, max_layer=3, downscale=2):
+def calc_pyramid(image, max_layer=3, downscale=2):
     pyramid = tuple(transform.pyramid_gaussian(
         image=image,
         max_layer=max_layer,
@@ -18,9 +19,11 @@ def pyramid(image, max_layer=3, downscale=2):
     ))
     return pyramid
 
-def pyramid_residuals(pyramid):
-    # First layer in pyramid is original image. Calculate residuals between
-    # filtered downsampled images subsequently rescaled and the original.
+def calc_pyramid_residuals(pyramid):
+    """
+    First layer in pyramid is original image. Calculate residuals between
+    filtered downsampled images subsequently rescaled and the original. 
+    """
     residuals = []
     for x in pyramid[1:]:
         x_resized = transform.resize(x, pyramid[0].shape)
@@ -32,7 +35,6 @@ def plot_pyramid_residuals(arg):
     pass
 
 def get_feature_vector(a):
-    # Calculate statistics
     feature_functions = [
         ('mean', numpy.mean),
         ('stdev', numpy.std),
@@ -50,7 +52,7 @@ def get_feature_vector(a):
 
 def get_pyramid_features(pyramid, pyramid_type):
     features = {}
-    for i, layer in enumerate(p):
+    for i, layer in enumerate(pyramid):
         feature_names, feature_vector = get_feature_vector(layer)
         for metric, value in zip(feature_names, feature_vector):
             feature_name = '{}_{}_{}'.format(pyramid_type, i, metric)
@@ -58,22 +60,66 @@ def get_pyramid_features(pyramid, pyramid_type):
     return features
 
 def gaussian_pyramid_features(image, max_layer=3, downscale=2):
-    p = pyramid(image, max_layer, downscale)
-    residuals = pyramid_residuals(p)
+    pyramid = calc_pyramid(image, max_layer, downscale)
+    residuals = calc_pyramid_residuals(pyramid)
 
-    features = get_pyramid_features(p, pyramid_type='gp')
+    features = get_pyramid_features(pyramid, pyramid_type='gp')
     features.update(get_pyramid_features(residuals, pyramid_type='gp_res'))
 
     return features
 
+def get_image_features(image):
+    features = gaussian_pyramid_features(image)
+    # features.update(other_features_here)
+    return features
+
+def create_image_feature_dataset(path_images, class_label, path_output):
+    print 'creating image feature dataset...'
+    dataset = list()
+    for i, filename in enumerate(os.listdir(path_images)):
+        image = io.imread(
+            fname='{}{}'.format(path_images, filename),
+            as_grey=True
+        )
+        features = get_image_features(image)
+        if i == 0:
+            feature_names = features.keys()
+
+        row = [filename, class_label]
+        for feature in feature_names:
+            row.append(features[feature])
+
+        dataset.append(row)
+
+        if i % 250 == 0:
+            print '{} images processed'.format(i)
+
+        if i > 1000:
+            break
+
+    df = pandas.DataFrame(dataset, columns=['image', 'label'] + feature_names)
+    df.to_csv(path_output, index)
+    print 'image feature dataset created.'
+
+
 if __name__ == '__main__':
     path = '/home/rokkuran/workspace/stegasawus'
-    path_images = '{}/images/originals/'.format(path)
-    filename = '19694.jpg'
 
-    image = io.imread(
-        fname='{}{}'.format(path_images, filename),
-        as_grey=True
+    def test_feature_generation():
+        path_images = '{}/images/originals/'.format(path)
+        filename = '19694.jpg'
+
+        image = io.imread(
+            fname='{}{}'.format(path_images, filename),
+            as_grey=True
+        )
+        features = gaussian_pyramid_features(image)
+        return features
+
+
+    path_cropped = '{}/images/validation/cropped/'.format(path)
+    create_image_feature_dataset(
+        path_images=path_cropped,
+        class_label='clean',
+        path_output='{}/data/cropped.csv'.format(path)
     )
-
-    features = gaussian_pyramid_features(image)
