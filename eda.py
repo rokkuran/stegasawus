c5b09af2761e73556c4365afebfb32e41eeb3ca5
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-21 22:35:13 +0000

    Slight modification of histogram plots of features.

--- eda.py
+++ eda.py
@@ -11,14 +11,17 @@ plt.show()
 
 
 path = '/home/rokkuran/workspace/stegasawus'
+path_train = '{}/data/train_catdog.csv'.format(path)
+train = pandas.read_csv(path_train)
 
+bins = 20
 for c in [x for x in train.columns if x not in ('label', 'image')]:
-    plt.hist(train[train.label=='clean'][c], bins=200, color='b', alpha=0.3, edgecolor='None')
-    plt.hist(train[train.label=='message'][c], bins=200, color='r', alpha=0.3, edgecolor='None')
-    plt.ylim([0, 50])
+    plt.hist(train[train.label=='cover'][c], bins=bins, color='b', alpha=0.3, edgecolor='None')
+    plt.hist(train[train.label=='stego'][c], bins=bins, color='r', alpha=0.3, edgecolor='None')
+    # plt.ylim([0, 50])
     plt.legend(loc='upper right')
     plt.title(c)
-    plt.savefig('{}/output/{}.png'.format(path, c))
+    plt.savefig('{}/output/{}_bins{}.png'.format(path, c, bins))
     print c
     plt.close()
 
