37f953853eedf04923c51d695fca5dde2dd0c577
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-30 22:23:41 +0000

    Plots - wavelet decomposition, rgb comparison and distplots .

--- eda.py
+++ eda.py
@@ -1,5 +1,6 @@
 import numpy
 import pandas
+import pywt
 
 from stegano import exifHeader
 from stegano import lsbset
@@ -11,6 +12,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import skimage.io as io
 
+# sns.set_style("white")
+sns.set_style("whitegrid", {'axes.grid' : False})
 
 #*******************************************************************************
 def rgb_to_grey(image):
@@ -58,10 +61,44 @@ class ImageComparer(object):
     def plot_images(self):
         io.imshow(numpy.concatenate((self.I, self.S), axis=1))
         plt.title('Left: original cover image. Right: steganographic image.')
+        plt.grid(False)
+        plt.show()
+
+    def plot_rgb_components(self):
+        """"""
+        # I_rgb = numpy.concatenate([self.I[:, :, c] for c in xrange(3)], axis=1)
+        # S_rgb = numpy.concatenate([self.S[:, :, c] for c in xrange(3)], axis=1)
+        # IS_rgb = numpy.concatenate((I_rgb, S_rgb), axis=0)
+        # io.imshow(IS_rgb)
+
+        f, axarr = plt.subplots(nrows=2, ncols=3)
+
+        axarr[0, 0].imshow(self.I[:, :, 0], cmap='Reds')
+        axarr[0, 0].set_title('cover red')
+        axarr[0, 1].imshow(self.I[:, :, 1], cmap='Greens')
+        axarr[0, 1].set_title('cover green')
+        axarr[0, 2].imshow(self.I[:, :, 2], cmap='Blues')
+        axarr[0, 2].set_title('cover blue')
+        axarr[1, 0].imshow(self.S[:, :, 0], cmap='Reds')
+        axarr[1, 0].set_title('stego red')
+        axarr[1, 1].imshow(self.S[:, :, 1], cmap='Greens')
+        axarr[1, 1].set_title('stego green')
+        axarr[1, 2].imshow(self.S[:, :, 2], cmap='Blues')
+        axarr[1, 2].set_title('stego blue')
+        # axarr[1, 0].imshow(self.I[:, :, 0], cmap='green')
+        # axarr[1, 0].set_title('Axis [1,0]')
+        # axarr[1, 1].scatter(x, y ** 2)
+        # axarr[1, 1].set_title('Axis [1,1]')
+
+        # title = '1st row: RGB components of cover image. | '
+        # title += '2nd row: RGB components of stego image.'
+        # plt.title(title)
+        # plt.grid(False)
         plt.show()
 
     def plot_difference(self, absolute=False):
         io.imshow(self.diff if not absolute else abs(self.diff))
+        plt.grid(False)
         plt.show()
 
 
@@ -86,6 +123,29 @@ def generate_feature_histograms(filepath_train, bins=50, normalise=False):
         plt.legend(loc='upper right', frameon=False)
         plt.title(feature)
         plt.savefig('{}/output/{}_bins{}.png'.format(path, feature, bins))
+        print feature
+        plt.close()
+
+
+def generate_feature_distplots(filepath_train, normalise=False):
+    """"""
+    train = pandas.read_csv(filepath_train)
+
+    cols = [x for x in train.columns if x not in ('label', 'image')]
+
+    if normalise:
+        train[cols] = train[cols].apply(lambda x: (x - x.mean()) / x.std())
+
+    for feature in cols:
+        label = 'cover'
+        sns.distplot(train[train.label==label][feature], label=label)
+
+        label = 'stego'
+        sns.distplot(train[train.label==label][feature], label=label)
+
+        plt.legend(loc='upper right', frameon=False)
+        plt.title(feature)
+        plt.savefig('{}/output/distplot_{}.png'.format(path, feature))
         print feature
         plt.close()
 
@@ -122,6 +182,25 @@ def generate_feature_kde(filepath_train, normalise=False):
 def plot_joint_dist_hex(x, y):
     sns.set(style="ticks")
     sns.jointplot(x, y, kind="hex", stat_func=stats.kendalltau, color="#4CB391")
+    plt.show()
+
+
+def plot_wavelet_decomposition(image, level=3):
+    coeffs = pywt.wavedec2(image, wavelet='haar', level=level)
+    for i, (cH, cV, cD) in enumerate(coeffs[1:]):
+        if i == 0:
+            cAcH = numpy.concatenate((coeffs[0], cH), axis=1)
+            cVcD = numpy.concatenate((cV, cD), axis=1)
+            plot_image = numpy.concatenate((cAcH, cVcD), axis=0)
+        else:
+            plot_image = numpy.concatenate((plot_image, cH), axis=1)
+            cVcD = numpy.concatenate((cV, cD), axis=1)
+            plot_image = numpy.concatenate((plot_image, cVcD), axis=0)
+
+    plt.grid(False)
+    # io.imshow(abs(plot_image), cmap='gray')
+    io.imshow(abs(plot_image), cmap='gray_r')
+    # io.imshow(plot_image)
     plt.show()
 
 
@@ -162,12 +241,13 @@ if __name__ == '__main__':
     # )
 
 
-    # path_cover = '{}images/png/cover/'.format(path)
-    # path_stego = '{}images/png/stego/'.format(path)
-    #
-    # filename = 'cat.13.png' # 96, 110, 224, 725
-    #
-    # z = ImageComparer(path_cover + filename, path_stego + filename)
+    path_cover = '{}images/png/cover/'.format(path)
+    path_stego = '{}images/png/stego/'.format(path)
+
+    filename = 'cat.2.png' # 96, 110, 224, 725
+
+    z = ImageComparer(path_cover + filename, path_stego + filename)
+    # plot_wavelet_decomposition(z.I[:, :, 0])
     # z.print_details()
     # # print z.reveal(generators.eratosthenes())
     # print z.reveal(generators.identity())
@@ -181,9 +261,11 @@ if __name__ == '__main__':
     # I = io.imread(path_cover + filename)
     # S = io.imread(path_stego + filename)
 
-    # generate_feature_histograms(
-    #     '{}data/train.csv'.format(path),
-    #     bins=50,
+    # generate_feature_distplots(
+    # # generate_feature_histograms(
+    #     # '{}data/train.csv'.format(path),
+    #     '{}data/train_wavelet.csv'.format(path),
+    #     # bins=50,
     #     normalise=False
     # )
     # generate_log_feature_histograms('{}data/train.csv'.format(path), bins=50)
