ec86a54651e885a1e6362ec1de662f2b582d6200
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-30 12:35:48 +0000

    Data analysis function revisions.

--- eda.py
+++ eda.py
@@ -1,61 +1,92 @@
-# plt.hist(train[train.label=='clean'].dwt_1_cV_mean, bins=200, color='b', alpha=0.3)
-# plt.hist(train[train.label=='message'].dwt_1_cV_mean, bins=200, color='r', alpha=0.3)
-# plt.hist(train[train.label=='clean'].dwt_1_cV_stdev, bins=200, color='b', alpha=0.3)
-# plt.hist(train[train.label=='message'].dwt_1_cV_stdev, bins=200, color='r', alpha=0.3)
-# plt.hist(train[train.label=='clean'].dwt_1_cV_skew, bins=200, color='b', alpha=0.3)
-# plt.hist(train[train.label=='message'].dwt_1_cV_skew, bins=200, color='r', alpha=0.3)
-plt.hist(train[train.label=='clean'].dwt_1_cV_kurtosis, bins=200, color='b', alpha=0.3)
-plt.hist(train[train.label=='message'].dwt_1_cV_kurtosis, bins=200, color='r', alpha=0.3)
-plt.ylim([0, 50])
-plt.show()
+import numpy
+
+from stegano import exifHeader
+from stegano import lsbset
+
+import matplotlib.pyplot as plt
+import skimage.io as io
+
+#*******************************************************************************
+def rgb_to_grey(image):
+    return numpy.dot(image, [0.2989, 0.5870, 0.1140])
+
+#*******************************************************************************
+class Comparer(object):
+    """"""
+    def __init__(self, filepath_cover, filepath_stego):
+        super(Comparer, self).__init__()
+        self._filepath_cover = filepath_cover
+        self._filepath_stego = filepath_stego
+
+        self.I = io.imread(filepath_cover)
+        self.S = io.imread(filepath_stego)
+
+    @property
+    def diff(self):
+        return self.I - self.S
+
+    @ property
+    def reveal(self):
+        return exifHeader.reveal(self._filepath_stego)
+
+    def print_details(self):
+        print 'cover_image: %s' % self._filepath_cover
+        print 'stego_image: %s' % self._filepath_stego
+        print 'message length = %s' % len(self.reveal)
+        a = numpy.sum(abs(self.diff))
+        print 'sum of absolute image difference = %s' % a
+
+    def plot_images(self):
+        io.imshow(numpy.concatenate((self.I, self.S), axis=1))
+        plt.title('Left: original cover image. Right: steganographic image.')
+        plt.show()
+
+    def plot_difference(self, absolute=False):
+        io.imshow(self.diff if not absolute else abs(self.diff))
+        plt.show()
 
 
-path = '/home/rokkuran/workspace/stegasawus'
-# path_train = '{}/data/train_catdog.csv'.format(path)
-path_train = '{}/data/train_catdog_ac.csv'.format(path)
-train = pandas.read_csv(path_train)
+#*******************************************************************************
+if __name__ == '__main__':
+    path = '/home/rokkuran/workspace/stegasawus/'
+    # path_cover = '{}images/train_catdog/cover/'.format(path)
+    # path_stego = '{}images/stego/catdog/'.format(path)
 
-bins = 20
-for c in [x for x in train.columns if x not in ('label', 'image')]:
-    plt.hist(train[train.label=='cover'][c], bins=bins, color='b', alpha=0.3, edgecolor='None')
-    plt.hist(train[train.label=='stego'][c], bins=bins, color='r', alpha=0.3, edgecolor='None')
-    # plt.ylim([0, 50])
-    plt.legend(loc='upper right')
-    plt.title(c)
-    plt.savefig('{}/output/{}_bins{}.png'.format(path, c, bins))
-    print c
-    plt.close()
+    # path_cover = '{}images/train/cropped/'.format(path)
+    # path_stego = '{}images/stego/paintings/'.format(path)
+
+    # filename = 'cat.698.jpg'
+    # filename = 'cat.445.jpg'
+    # filename = '10.jpg'
+    # plot_images(filename, path_cover, path_stego)
+
+    # Z = Comparer(path_cover + filename, path_stego + filename)
+    # Z = Comparer(
+    #     '/home/rokkuran/workspace/stegasawus/images/Lenna.jpg',
+    #     '/home/rokkuran/workspace/stegasawus/images/image.jpg'
+    # )
+
+    # Z.print_details()
+    # if numpy.sum(abs(Z.diff)) > 0:
+    #     Z.plot_images()
+    #     Z.plot_difference()
+
+    # exifHeader.hide(
+    #     "{}images/Lenna.jpg".format(path),
+    #     "{}images/Lenna_stego.jpg".format(path),
+    #     secret_message="Hello world!"
+    # )
+    #
+    # Z = Comparer(
+    #     "{}images/Lenna.jpg".format(path),
+    #     "{}images/Lenna_stego.jpg".format(path)
+    # )
 
 
-hist1, bin_edges1 = numpy.histogram(train[train.label=='clean'][c], bins=100)
-# plt.bar(bin_edges1[:-1], hist1, color='b', alpha=0.5, edgecolor='None', width=0.5)
+    path_cover = '{}images/png/cover/'.format(path)
+    path_stego = '{}images/png/stego/'.format(path)
 
-hist2, bin_edges2 = numpy.histogram(train[train.label=='message'][c], bins=100)
-# plt.bar(bin_edges2[:-1], hist2, color='r', alpha=0.5, edgecolor='None', width=0.5)
+    filename = 'cat.445.png'
 
-hist3 = hist2 - hist1
-plt.bar(bin_edges2[:-1], hist3, color='r', alpha=0.5, edgecolor='None', width=0.5)
-
-bin_span = numpy.concatenate((bin_edges1, bin_edges2))
-plt.xlim(bin_span.min(), bin_span.max())
-plt.show()
-
-
-path = '/home/rokkuran/workspace/stegasawus'
-path_train = '{}/data/train_catdog_rndembed_ac.csv'.format(path)
-train = pandas.read_csv(path_train)
-
-lc = plt.scatter(
-    # train.aca_01[train.label=='cover'],
-    train.ac_01_mean[train.label=='cover'],
-    train.ac_01_stdev[train.label=='cover'],
-    marker='o', color='b', alpha=0.66
-)
-ls = plt.scatter(
-    # train.aca_01[train.label=='stego'],
-    train.ac_01_mean[train.label=='stego'],
-    train.ac_01_stdev[train.label=='stego'],
-    marker='o', color='r', alpha=0.66
-)
-plt.legend((lc, ls), ('cover', 'stego'))
-plt.show()
+    I = io.imread(path_cover + filename)
+    S = io.imread(path_stego + filename)
