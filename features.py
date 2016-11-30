0ec58d03039ff19e8a205b9c30b585345320b0e4
rokkkuran <lachlanbtaylor@gmail.com>
2016-11-30 22:24:28 +0000

    Wavelet decomposition feature vectors function.

--- features.py
+++ features.py
@@ -1,6 +1,7 @@
 import numpy
 import pandas
 import os
+import pywt
 
 from scipy import stats
 import matplotlib.pyplot as plt
@@ -103,6 +104,78 @@ def create_training_set(filepath_cover, filepath_stego, path_output):
     return train
 
 
+def get_wavdec_feature_vector(coeffs):
+    feature_functions = [
+        ('mean', numpy.mean),
+        ('stdev', numpy.std),
+        ('skew', stats.skew),
+        ('kurtosis', stats.kurtosis)]
+
+    feature_vector = {}
+    cA = coeffs[0]
+    for (fname, fn) in feature_functions:
+        feature_name = 'dwt_{layer}_cA_{fname}'.format(
+            layer=len(coeffs) - 1, fname=fname
+        )
+        # reduce sensitivity to noise
+        c_tol = abs(cA) > 1 # coefficients with magnitude > 1 allowed
+        if c_tol.any():
+            feature_vector[feature_name] = fn(cA[c_tol].flatten())
+        else:
+            feature_vector[feature_name] = 0
+
+    for i, (cH, cV, cD) in enumerate(coeffs[1:]):
+        layer = len(coeffs) - 1 - i
+
+        for (fname, fn) in feature_functions:
+            for c, cX in zip(('cH', 'cV', 'cD'), (cH, cV, cD)):
+                feature_name = 'dwt_{layer}_{c}_{fname}'.format(
+                    layer=layer, c=c, fname=fname
+                )
+                c_tol = abs(cX) > 1
+                if c_tol.any():
+                    feature_vector[feature_name] = fn(cX[c_tol].flatten())
+                else:
+                    feature_vector[feature_name] = 0
+
+    return feature_vector
+
+
+def create_image_wavelet_feature_dataset(path_images, class_label, path_output,
+    image_limit=None):
+    """Create autocorrelation feature set from images."""
+
+    print 'creating image feature dataset...'
+
+    dataset = list()
+    for i, filename in enumerate(os.listdir(path_images)):
+        fname = '{}{}'.format(path_images, filename)
+        # image = io.imread(fname, as_grey=True)
+        image = io.imread(fname)
+
+        image_greyscale = rgb_to_grey(image)
+        coeffs = pywt.wavedec2(image_greyscale, wavelet='haar', level=3)
+        features = get_wavdec_feature_vector(coeffs)
+        if i == 0:
+            feature_names = features.keys()
+
+        row = [filename, class_label]
+        for feature in feature_names:
+            row.append(features[feature])
+        dataset.append(row)
+
+        if i % 250 == 0:
+            print '{} images processed'.format(i)
+
+        if image_limit:
+            if i > image_limit:
+                break
+
+    df = pandas.DataFrame(dataset, columns=['image', 'label'] + feature_names)
+    df.to_csv(path_output, index=False)
+
+    print 'image feature dataset created.'
+
 #*******************************************************************************
 if __name__ == '__main__':
     path = '/home/rokkuran/workspace/stegasawus/'
@@ -112,20 +185,32 @@ if __name__ == '__main__':
     path_cover = '{}images/png/cover/'.format(path)
     path_stego = '{}images/png/stego/'.format(path)
 
-    create_image_ac_feature_dataset(
+    # create_image_ac_feature_dataset(
+    #     path_images=path_cover,
+    #     class_label='cover',
+    #     path_output='{}data/train_cover.csv'.format(path)
+    # )
+    #
+    # create_image_ac_feature_dataset(
+    #     path_images=path_stego,
+    #     class_label='stego',
+    #     path_output='{}data/train_stego.csv'.format(path)
+    # )
+
+    create_image_wavelet_feature_dataset(
         path_images=path_cover,
         class_label='cover',
-        path_output='{}data/train_cover.csv'.format(path)
+        path_output='{}data/train_cover_wavelet.csv'.format(path)
     )
 
-    create_image_ac_feature_dataset(
+    create_image_wavelet_feature_dataset(
         path_images=path_stego,
         class_label='stego',
-        path_output='{}data/train_stego.csv'.format(path)
+        path_output='{}data/train_stego_wavelet.csv'.format(path)
     )
 
     create_training_set(
-        '{}data/train_cover.csv'.format(path),
-        '{}data/train_stego.csv'.format(path),
-        '{}data/train.csv'.format(path)
+        '{}data/train_cover_wavelet.csv'.format(path),
+        '{}data/train_stego_wavelet.csv'.format(path),
+        '{}data/train_wavelet.csv'.format(path)
     )
