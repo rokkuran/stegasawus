a358f5a60c8ee3b003a7a70218917fccb8e89715
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-01 23:31:06 +0000

    Shifted parameter tuning dictionary to yaml file.

--- stegasawus/model.py
+++ stegasawus/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import yaml
 
 import matplotlib.pyplot as plt
 from scipy import stats
@@ -13,8 +14,7 @@ from sklearn.preprocessing import (
 from sklearn.model_selection import (
     GridSearchCV,
     learning_curve,
-    ShuffleSplit,
-    StratifiedShuffleSplit)
+    ShuffleSplit)
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.decomposition import PCA, KernelPCA
 from sklearn.feature_selection import SelectKBest, RFE
@@ -48,6 +48,9 @@ def model_parameter_tuning(clf, X_train, y_train, parameters, scoring, cv=5):
     best_parameters, score, _ = max(gs_clf.grid_scores_, key=lambda x: x[1])
     for param_name in sorted(parameters.keys()):
         print("%s: %r" % (param_name, best_parameters[param_name]))
+
+
+# TODO: fixup and include particle swarm optimisation parameter tuning
 
 
 def scoring_metrics(y_pred, y_true, return_string=False):
@@ -104,20 +107,20 @@ if __name__ == '__main__':
 
     # **************************************************************************
     classifiers = {
-        # 'knn': KNeighborsClassifier(
-        #     n_neighbors=6,
-        #     algorithm='ball_tree',
-        #     weights='distance',
-        #     metric='chebyshev'
-        # ),
-        # 'knn_default': KNeighborsClassifier(),
-        # 'svc_rbf': SVC(
-        #     kernel='rbf',
-        #     C=50,
-        #     gamma=0.01,
-        #     tol=1e-3
-        # ),
-        # 'svc_rbf_default': SVC(kernel='rbf'),
+        'knn': KNeighborsClassifier(
+            n_neighbors=6,
+            algorithm='ball_tree',
+            weights='distance',
+            metric='chebyshev'
+        ),
+        'knn_default': KNeighborsClassifier(),
+        'svc_rbf': SVC(
+            kernel='rbf',
+            C=50,
+            gamma=0.01,
+            tol=1e-3
+        ),
+        'svc_rbf_default': SVC(kernel='rbf'),
         'svc_linear': LinearSVC(
             C=1e3,
             loss='squared_hinge',
@@ -125,93 +128,47 @@ if __name__ == '__main__':
             tol=1e-3
         ),
         'svc_linear_default': LinearSVC(),
-        # 'nusvc': NuSVC(),
-        # 'rf': RandomForestClassifier(
-        #     criterion='entropy',
-        #     max_depth=12,
-        #     min_samples_leaf=8,
-        #     min_samples_split=5
-        # ),
-        # 'rf_default': RandomForestClassifier(),
-        # 'xgb': XGBClassifier(),
-        # 'adaboost': AdaBoostClassifier(),
-        # 'et': ExtraTreesClassifier(
-        #     criterion='entropy',
-        #     max_depth=25,
-        #     min_samples_leaf=5,
-        #     min_samples_split=5
-        # ),
-        # 'et_default': ExtraTreesClassifier(),
-        # 'gbc': GradientBoostingClassifier(),
+        'nusvc': NuSVC(),
+        'rf': RandomForestClassifier(
+            criterion='entropy',
+            max_depth=12,
+            min_samples_leaf=8,
+            min_samples_split=5
+        ),
+        'rf_default': RandomForestClassifier(),
+        'xgb': XGBClassifier(),
+        'adaboost': AdaBoostClassifier(),
+        'et': ExtraTreesClassifier(
+            criterion='entropy',
+            max_depth=25,
+            min_samples_leaf=5,
+            min_samples_split=5
+        ),
+        'et_default': ExtraTreesClassifier(),
+        'gbc': GradientBoostingClassifier(),
         'lr_lbfgs': LogisticRegression(
             # C=1000,
             # tol=1e-3,
-            C=3.23594105e+01,
+            C=3.23594105e+01,  # particle swarm optimised
             tol=6.83049831e-04,
             solver='lbfgs'
         ),
         'lr_lbfgs_default': LogisticRegression(),
-        # 'pa': PassiveAggressiveClassifier(
-        #     C=0.01,
-        #     fit_intercept=True,
-        #     loss='hinge'
-        # ),
-        # 'pa_default': PassiveAggressiveClassifier(),
-        # 'gnb': GaussianNB(),
-        # 'lda': LinearDiscriminantAnalysis(),
-        # 'qda': QuadraticDiscriminantAnalysis(),
+        'pa': PassiveAggressiveClassifier(
+            C=0.01,
+            fit_intercept=True,
+            loss='hinge'
+        ),
+        'pa_default': PassiveAggressiveClassifier(),
+        'gnb': GaussianNB(),
+        'lda': LinearDiscriminantAnalysis(),
+        'qda': QuadraticDiscriminantAnalysis(),
     }
 
     # **************************************************************************
-    parameters = {
-        'svc_rbf': {
-            'svc_rbf__C': [1, 50, 100, 250, 600, 650, 750, 800, 900, 1000],
-            'svc_rbf__tol': [1e-3, 1e-4],
-            'svc_rbf__gamma': [0.01, 0.1, 0.25, 0.5, 0.75],
-        },
-        'svc_linear': {
-            'svc_linear__C': np.logspace(-2, 3, 6),
-            'svc_linear__tol': [1e-3, 1e-4],
-        },
-        'nusvc': {
-            'nusvc__nu': [0.01, 0.1, 0.25, 0.5, 0.75, 0.90],
-            'nusvc__kernel': ['rbf', 'poly', 'sigmoid'],
-            'nusvc__tol': [1e-3, 1e-4],
-            'nusvc__gamma': [0.01, 0.1, 0.25, 0.5, 0.75],
-        },
-        'knn': {
-            'knn__n_neighbors': [3, 6, 9],
-            'knn__weights': ['uniform', 'distance'],
-            'knn__algorithm': ['ball_tree', 'kd_tree', 'brute'],
-            'knn__metric': ['minkowski', 'euclidean', 'chebyshev', 'manhattan']
-        },
-        'bag_knn': {
-            'bag_knn__base_estimator__n_neighbors': [3, 6, 9],
-            'bag_knn__max_samples': [0.1, 0.25, 0.5, 0.75, 0.90],
-            'bag_knn__max_features': [0.1, 0.25, 0.5, 0.75, 0.90],
-        },
-        'lr_lbfgs': {
-            'lr_lbfgs__C': np.logspace(-3, 4, 8),
-            'lr_lbfgs__tol': [1e-3, 1e-4]
-        },
-        'rf': {
-            'rf__criterion': ['gini', 'entropy'],
-            'rf__max_depth': [10, 12, 15, 20],
-            'rf__min_samples_leaf': [1, 5, 8, 12],
-            'rf__min_samples_split': [2, 3, 4, 5],
-        },
-        'et': {
-            'et__criterion': ['gini', 'entropy'],
-            'et__max_depth': [6, 10, 18, 20, 25],
-            'et__min_samples_leaf': [1, 5, 8, 12],
-            'et__min_samples_split': [2, 3, 5, 8, 10],
-        },
-        'pa': {
-            'pa__C': np.logspace(-3, 4, 8),
-            'pa__fit_intercept': [False, True],
-            'pa__loss': ['hinge', 'squared_hinge'],
-        }
-    }
+    parameters = yaml.safe_load(
+        open('{}/stegasawus/parameter_tuning.yaml'.format(path), 'rb')
+    )
 
     # **************************************************************************
     # name = 'svc_linear'
@@ -236,14 +193,14 @@ if __name__ == '__main__':
         'f1', 'roc_auc'
     ]
 
-    sss = StratifiedShuffleSplit(
+    ss = ShuffleSplit(
         n_splits=5,
         test_size=0.2,
         random_state=0
     )
 
     train = train.as_matrix()
-    for i, (train_idx, val_idx) in enumerate(sss.split(train, y_train_binary)):
+    for i, (train_idx, val_idx) in enumerate(ss.split(train, y_train_binary)):
         X_train, X_val = train[train_idx], train[val_idx]
         y_train, y_val = y_train_binary[train_idx], y_train_binary[val_idx]
 
@@ -267,12 +224,10 @@ if __name__ == '__main__':
         by=['acc', 'f1', 'roc_auc'],
         ascending=False
     ).reset_index(drop=True)
-    # print '\n', scores.head(10)
 
     scores_mean = scores.ix[:, scores.columns != 'split'] \
         .groupby(['classifier']) \
         .mean() \
         .sort_values(by=['acc', 'f1', 'roc_auc'], ascending=False) \
-        # .reset_index()
 
     print '\n\n', scores_mean
