bd744a507356d17db0fb69bb3a05f5110a778cdf
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-01 15:05:36 +0000

    Combined autocorrelation and wavelet transform feature sets.

--- stegasawus/features.py
+++ stegasawus/features.py
@@ -193,22 +193,95 @@ def wavdec_features(coeffs, tol=1):
     return features
 
 
+def rgb_wavelet_features(I, tol=1):
+    """"""
+    features = {}
+    m, n, _ = I.shape
+
+    for c, colour in enumerate('rgb'):
+        coeffs = pywt.wavedec2(I[:, :, c], wavelet='haar', level=3)
+        f_wavelet = wavdec_features(coeffs)
+        f_wavelet = prefix_dict_keys(f_wavelet, colour)
+        features.update(f_wavelet)
+
+    return features
+
 
 def wavelet_feature_dataset(path_images, class_label, path_output,
     image_limit=None):
-    """Create autocorrelation feature set from images."""
 
     print 'creating image feature dataset...'
 
     dataset = list()
     for i, filename in enumerate(os.listdir(path_images)):
         fname = '{}{}'.format(path_images, filename)
-        # image = io.imread(fname, as_grey=True)
         image = io.imread(fname)
 
-        image_greyscale = rgb_to_grey(image)
-        coeffs = pywt.wavedec2(image_greyscale, wavelet='haar', level=3)
-        features = wavdec_features(coeffs)
+        features = rgb_wavelet_features(image)
+        if i == 0:
+            feature_names = features.keys()
+
+        row = [filename, class_label]
+        for feature in feature_names:
+            row.append(features[feature])
+        dataset.append(row)
+
+        if i % 250 == 0:
+            print '{} images processed'.format(i)
+
+        if image_limit:
+            if i > image_limit:
+                break
+
+    df = pd.DataFrame(dataset, columns=['image', 'label'] + feature_names)
+    df.to_csv(path_output, index=False)
+
+    print 'image feature dataset created.'
+
+
+def create_feature_dataset(path_images, class_label, path_output,
+    f_types=['autocorrelation', 'wavelet'], image_limit=None):
+
+    """
+    Create feature vectors from images in directory and save as csv output.
+
+    Parameters
+    ----------
+    path_images : directory path string
+        Directory with images for processing.
+    class_label : string
+        Class label used in label column of output.
+    path_output : directory path string
+        Output directory for csv file.
+    f_types : array_like, optional
+        Specify the feature types:
+            - 'autocorrelation'
+            - 'wavelet'
+        Default: ['autocorrelation', 'wavelet']
+    image_limit : int, optional
+        Number of images in directory to process.
+
+    Returns
+    -------
+    csv output file as specified in path_output.
+
+    """
+
+    print 'creating image feature dataset...'
+
+    dataset = list()
+    for i, filename in enumerate(os.listdir(path_images)):
+        fname = '{}{}'.format(path_images, filename)
+        image = io.imread(fname)
+
+        features = {}
+        if 'autocorrelation' in f_types:
+            lags = ((1, 0), (0, 1), (1, 1), (1, 2), (2, 2), (2, 2))
+            features.update(rgb_autocorrelation_features(image, lags))
+
+        if 'wavelet' in f_types:
+            features.update(rgb_wavelet_features(image))
+
         if i == 0:
             feature_names = features.keys()
 
@@ -238,7 +311,7 @@ if __name__ == '__main__':
 
     path_cover = '{}images/png/cover/'.format(path)
     path_stego = '{}images/png/stego/'.format(path)
-    #
+
     # autocorrelation_feature_dataset(
     #     path_images=path_cover,
     #     class_label='cover',
@@ -258,12 +331,12 @@ if __name__ == '__main__':
     # )
 
     #***************************************************************************
-    wavelet_feature_dataset(
-        path_images=path_cover,
-        class_label='cover',
-        path_output='{}data/train_cover_wavelet.csv'.format(path)
-    )
-
+    # wavelet_feature_dataset(
+    #     path_images=path_cover,
+    #     class_label='cover',
+    #     path_output='{}data/train_cover_wavelet.csv'.format(path)
+    # )
+    #
     # wavelet_feature_dataset(
     #     path_images=path_stego,
     #     class_label='stego',
@@ -275,3 +348,24 @@ if __name__ == '__main__':
     #     '{}data/train_stego_wavelet.csv'.format(path),
     #     '{}data/train_wavelet.csv'.format(path)
     # )
+
+    #***************************************************************************
+    create_feature_dataset(
+        path_images=path_cover,
+        class_label='cover',
+        path_output='{}data/train_cover.csv'.format(path),
+        f_types=['autocorrelation', 'wavelet']
+    )
+
+    create_feature_dataset(
+        path_images=path_stego,
+        class_label='stego',
+        path_output='{}data/train_stego.csv'.format(path),
+        f_types=['autocorrelation', 'wavelet']
+    )
+
+    create_training_set(
+        '{}data/train_cover.csv'.format(path),
+        '{}data/train_stego.csv'.format(path),
+        '{}data/train.csv'.format(path)
+    )
