4186b01debbcd260c0606900cb30cbaaff2cca16
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-01 14:32:25 +0000

    Feature creation refactoring: autocorrelation and wavelet transforms.

--- stegasawus/features.py
+++ stegasawus/features.py
@@ -1,5 +1,5 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import os
 import pywt
 
@@ -23,39 +23,36 @@ def rgb_to_grey(image):
         Greyscale image array from RGB
 
     """
-    return numpy.dot(image, [0.2989, 0.5870, 0.1140])
+    return np.dot(image, [0.2989, 0.5870, 0.1140])
 
 
-def statistical_metrics(values, name):
+def statistical_metrics(x):
     """
-    Calculates statistical metrics from array (mean, std, skew, kurtosis).
+    Calculates statistical metrics on input array (mean, std, skew, kurtosis).
 
     Parameters
     ----------
-    values : numpy.ndarray
+    x : numpy.ndarray
         Array to compute statics on.
-
-    name : string
-        Name to prefix metrics in dict output.
 
     Returns
     -------
     features : dict
-        Dictionary of metrics with keys of form 'name_metric'
+        Dictionary of metrics.
 
     """
-    metrics = (
-        ('mean', numpy.mean),
-        ('stdev', numpy.std),
-        ('skew', stats.skew),
-        ('kurtosis', stats.kurtosis)
-    )
 
-    features = {}
-    for f, fn in metrics:
-        features['{}_{}'.format(name, f)] = fn(values.flatten())
+    metrics = {
+        'mean': np.mean,
+        'stdev': np.std,
+        'skew': stats.skew,
+        'kurtosis': stats.kurtosis
+    }
+    return {k: fn(x.flatten()) for k, fn in metrics.items()}
 
-    return features
+
+def prefix_dict_keys(d, prefix):
+    return {'{}_{}'.format(prefix, k): v for k, v in d.items()}
 
 
 def autocorrelation_features(I, lags=((1, 0), (0, 1))):
@@ -75,16 +72,19 @@ def autocorrelation_features(I, lags=((1, 0), (0, 1))):
     features : dict
 
     """
-    features = {}
+
     m, n = I.shape
 
+    features = {}
     for x, y in lags:
         ac = I[x:, y:] * I[:m-x, :n-y]
-        aca = ac / (I[x:, y:].std() * I[:m-x, :n-y].std())
-        features['aca_{}{}'.format(x, y)] = aca.sum()
+        aca = np.sum(ac / (I[x:, y:].std() * I[:m-x, :n-y].std()))
 
-        sm = statistical_metrics(values=ac, name='ac_{}{}'.format(x, y))
-        features.update(sm)
+        features['aca_{}{}'.format(x, y)] = aca
+
+        f_stat = statistical_metrics(ac)
+        f_stat = prefix_dict_keys(f_stat, 'ac_{}{}'.format(x, y))
+        features.update(f_stat)
 
     return features
 
@@ -110,13 +110,9 @@ def rgb_autocorrelation_features(I, lags=((1, 0), (0, 1))):
     m, n, _ = I.shape
 
     for c, colour in enumerate('rgb'):
-        for x, y in lags:
-            ac = I[x:, y:, c] * I[:m-x, :n-y, c]
-            aca = ac / (I[x:, y:, c].std() * I[:m-x, :n-y, c].std())
-            features['aca_{}_{}{}'.format(colour, x, y)] = aca.sum()
-
-            sm = statistical_metrics(ac, 'ac_{}_{}{}'.format(colour, x, y))
-            features.update(sm)
+        f_ac = autocorrelation_features(I[:, :, c], lags)
+        f_ac = prefix_dict_keys(f_ac, colour)
+        features.update(f_ac)
 
     return features
 
@@ -149,7 +145,7 @@ def autocorrelation_feature_dataset(path_images, class_label, path_output,
             if i > image_limit:
                 break
 
-    df = pandas.DataFrame(dataset, columns=['image', 'label'] + feature_names)
+    df = pd.DataFrame(dataset, columns=['image', 'label'] + feature_names)
     df.to_csv(path_output, index=False)
 
     print 'image feature dataset created.'
@@ -157,76 +153,44 @@ def autocorrelation_feature_dataset(path_images, class_label, path_output,
 
 def create_training_set(filepath_cover, filepath_stego, path_output):
     """"""
-    train_cover = pandas.read_csv(filepath_cover)
-    train_stego = pandas.read_csv(filepath_stego)
-    train = pandas.concat([train_cover, train_stego])
+    train_cover = pd.read_csv(filepath_cover)
+    train_stego = pd.read_csv(filepath_stego)
+    train = pd.concat([train_cover, train_stego])
     train.to_csv(path_output, index=False)
     return train
 
 
-def get_wavdec_feature_vector(coeffs):
-    feature_functions = [
-        ('mean', numpy.mean),
-        ('stdev', numpy.std),
-        ('skew', stats.skew),
-        ('kurtosis', stats.kurtosis)]
-
-    feature_vector = {}
-
-    cA = coeffs[0]
-    for (fname, fn) in feature_functions:
-        feature_name = 'dwt_{layer}_cA_{fname}'.format(
-            layer=len(coeffs) - 1, fname=fname
-        )
-        # reduce sensitivity to noise
-        c_tol = abs(cA) > 1 # coefficients with magnitude > 1 allowed
-        if c_tol.any():
-            feature_vector[feature_name] = fn(cA[c_tol].flatten())
-        else:
-            feature_vector[feature_name] = 0
-
-    for i, (cH, cV, cD) in enumerate(coeffs[1:]):
-        layer = len(coeffs) - 1 - i
-
-        for (fname, fn) in feature_functions:
-            for c, cX in zip(('cH', 'cV', 'cD'), (cH, cV, cD)):
-                feature_name = 'dwt_{layer}_{c}_{fname}'.format(
-                    layer=layer, c=c, fname=fname
-                )
-                c_tol = abs(cX) > 1
-                if c_tol.any():
-                    feature_vector[feature_name] = fn(cX[c_tol].flatten())
-                else:
-                    feature_vector[feature_name] = 0
-
-    return feature_vector
-
-
-def wavdec_feature_vector(coeffs):
-    """"""
-    feature_vector = {}
-
-    cA = coeffs[0]
-    name = 'dwt_{layer}_cA'.format(layer=len(coeffs) - 1)
-
-    # reduce sensitivity to noise
-    c_tol = abs(cA) > 1 # coefficients with magnitude > 1 allowed
-    if c_tol.any():
-        feature_vector.update(statistical_metrics(cA[c_tol], name))
+def apply_tolerance(x, tol):
+    x_tol = abs(x) > tol
+    if x_tol.any():
+        return x[x_tol]
     else:
-        feature_vector.update(statistical_metrics(numpy.zeros(1), name))
+        return np.zeros(1)
+
+
+def wavdec_features(coeffs, tol=1):
+    """"""
+    n_layers = len(coeffs) - 1
+
+    features = {}
+
+    cA = coeffs[0]
+    prefix = 'dwt_{}_cA'.format(n_layers)
+    cA = apply_tolerance(cA, tol) # reduce sensitivity to noise
+    f_stat = statistical_metrics(cA)
+    f_stat = prefix_dict_keys(f_stat, prefix)
+    features.update(f_stat)
 
     for i, (cH, cV, cD) in enumerate(coeffs[1:]):
-        layer = len(coeffs) - 1 - i
+        layer = n_layers - i
         for c, cX in zip(('cH', 'cV', 'cD'), (cH, cV, cD)):
-            name = 'dwt_{layer}_{c}'.format(layer=layer, c=c)
-            c_tol = abs(cX) > 1
-            if c_tol.any():
-                feature_vector.update(statistical_metrics(cX[c_tol], name))
-            else:
-                feature_vector.update(statistical_metrics(numpy.zeros(1), name))
+            prefix = 'dwt_{}_{}'.format(layer, c)
+            cX = apply_tolerance(cX, tol)
+            f_stat = statistical_metrics(cX)
+            f_stat = prefix_dict_keys(f_stat, prefix)
+            features.update(f_stat)
 
-    return feature_vector
+    return features
 
 
 
@@ -244,7 +208,7 @@ def wavelet_feature_dataset(path_images, class_label, path_output,
 
         image_greyscale = rgb_to_grey(image)
         coeffs = pywt.wavedec2(image_greyscale, wavelet='haar', level=3)
-        features = wavdec_feature_vector(coeffs)
+        features = wavdec_features(coeffs)
         if i == 0:
             feature_names = features.keys()
 
@@ -260,7 +224,7 @@ def wavelet_feature_dataset(path_images, class_label, path_output,
             if i > image_limit:
                 break
 
-    df = pandas.DataFrame(dataset, columns=['image', 'label'] + feature_names)
+    df = pd.DataFrame(dataset, columns=['image', 'label'] + feature_names)
     df.to_csv(path_output, index=False)
 
     print 'image feature dataset created.'
@@ -274,33 +238,40 @@ if __name__ == '__main__':
 
     path_cover = '{}images/png/cover/'.format(path)
     path_stego = '{}images/png/stego/'.format(path)
-
+    #
     # autocorrelation_feature_dataset(
     #     path_images=path_cover,
     #     class_label='cover',
-    #     path_output='{}data/train_cover.csv'.format(path)
+    #     path_output='{}data/train_cover_ac.csv'.format(path)
     # )
     #
     # autocorrelation_feature_dataset(
     #     path_images=path_stego,
     #     class_label='stego',
-    #     path_output='{}data/train_stego.csv'.format(path)
+    #     path_output='{}data/train_stego_ac.csv'.format(path)
     # )
 
+    # create_training_set(
+    #     '{}data/train_cover_ac.csv'.format(path),
+    #     '{}data/train_stego_ac.csv'.format(path),
+    #     '{}data/train_ac.csv'.format(path)
+    # )
+
+    #***************************************************************************
     wavelet_feature_dataset(
         path_images=path_cover,
         class_label='cover',
         path_output='{}data/train_cover_wavelet.csv'.format(path)
     )
 
-    wavelet_feature_dataset(
-        path_images=path_stego,
-        class_label='stego',
-        path_output='{}data/train_stego_wavelet.csv'.format(path)
-    )
-
-    create_training_set(
-        '{}data/train_cover_wavelet.csv'.format(path),
-        '{}data/train_stego_wavelet.csv'.format(path),
-        '{}data/train_wavelet.csv'.format(path)
-    )
+    # wavelet_feature_dataset(
+    #     path_images=path_stego,
+    #     class_label='stego',
+    #     path_output='{}data/train_stego_wavelet.csv'.format(path)
+    # )
+    #
+    # create_training_set(
+    #     '{}data/train_cover_wavelet.csv'.format(path),
+    #     '{}data/train_stego_wavelet.csv'.format(path),
+    #     '{}data/train_wavelet.csv'.format(path)
+    # )
