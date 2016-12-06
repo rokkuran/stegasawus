b50764ef850acd17d5fa4339dcbc1cff3673f313
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-06 18:02:16 +0000

    Refactored data splitting cv and model comparisons.

--- stegasawus/model.py
+++ stegasawus/model.py
@@ -25,8 +25,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import (
     LogisticRegression,
     PassiveAggressiveClassifier)
-from sklearn.svm import SVC, LinearSVC, NuSVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import (
     RandomForestClassifier,
     ExtraTreesClassifier,
@@ -34,29 +33,183 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     VotingClassifier)
 from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import (
-    LinearDiscriminantAnalysis,
-    QuadraticDiscriminantAnalysis)
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 from xgboost import XGBClassifier
 
 
+def cv_split_generator(X, y, splitter):
+    """
+    Train and validation set split generator.
+
+    Parameters
+    ----------
+    X : numpy.ndarray
+        Feature data.
+    y : numpy.ndarray
+        Target data.
+    splitter : sklearn splitter
+        - ShuffleSplit
+        - StratifiedShuffleSplit
+
+    Yields
+    ------
+    i, X_train, X_val, y_train, y_val : data split
+
+    i : int
+        Iteration count.
+    X_train : numpy.ndarray
+        Feature training data.
+    X_val : numpy.ndarray
+        Feature validation data.
+    y_train : numpy.ndarray
+        Target training data.
+    y_val : numpy.ndarray
+        Target validation set.
+
+    """
+    for i, (train_idx, val_idx) in enumerate(splitter.split(X, y)):
+        X_train, X_val = X[train_idx], X[val_idx]
+        y_train, y_val = y[train_idx], y[val_idx]
+        yield i, X_train, X_val, y_train, y_val
+
+
 def scoring_metrics(y_true, y_pred, return_string=False):
     acc = metrics.accuracy_score(y_true, y_pred)
+    ll = metrics.log_loss(y_true, y_pred)
     p = metrics.precision_score(y_true, y_pred)
     r = metrics.recall_score(y_true, y_pred)
     f1 = metrics.f1_score(y_true, y_pred)
     roc_auc = metrics.roc_auc_score(y_true, y_pred)
 
-    scores = [acc, p, r, f1, roc_auc]
+    scores = [acc, ll, p, r, f1, roc_auc]
 
-    s = 'acc = {:.2%}; p = {:.4f}; r = {:.4f}; '.format(acc, p, r)
-    s += 'f1 = {:.4f}; roc_auc = {:.4f}'.format(f1, roc_auc)
+    s = 'acc = {:.2%}; ll = {:.4f}; p = {:.4f}; '.format(acc, ll, p)
+    s += 'r = {:.4f}; f1 = {:.4f}; roc_auc = {:.4f}'.format(r, f1, roc_auc)
 
     if return_string:
         return scores, s
     else:
         return scores
+
+
+def get_pipeline(name):
+    combined_features = Pipeline([
+        # ('features', FeatureUnion([
+        #     ('scaler', StandardScaler()),
+        #     # ('poly', PolynomialFeatures(
+        #     #     degree=2,
+        #     #     interaction_only=True,
+        #     #     include_bias=False
+        #     # ))
+        # ])),
+        ('pca', Pipeline([
+            ('scaler', StandardScaler()),
+            ('pca', PCA(n_components=125)),
+        ])),
+        # ('kpca', KernelPCA(n_components=10)),
+    ])
+    pipeline = Pipeline([
+        ('features', combined_features),
+        (name, classifiers[name])
+    ])
+    return pipeline
+
+
+def model_comparison(classifiers, X, y, splitter, cv_mean=True):
+    scores = []
+    score_cols = [
+        'classifier', 'split', 'acc', 'log_loss',
+        'precision', 'recall', 'f1', 'roc_auc']
+
+    cv_splits = cv_split_generator(X=X, y=y, splitter=splitter)
+
+    for i, X_train, X_val, y_train, y_val in cv_splits:
+        for name, clf in classifiers.iteritems():
+            pipeline = get_pipeline(name)
+            estimator = pipeline.fit(X_train, y_train)
+
+            y_pred = estimator.predict(X_val)
+            m, ps = scoring_metrics(y_val, y_pred, return_string=True)
+            ps += ' | {}_{}'.format(name, i)
+            print ps
+            scores.append([name, i] + m)
+
+    scores = pd.DataFrame(scores, columns=score_cols)
+    scores = scores.sort_values(
+        by=['acc', 'log_loss'],
+        ascending=[False, True]
+    ).reset_index(drop=True)
+
+    scores_mean = scores.ix[:, scores.columns != 'split'] \
+        .groupby(['classifier']) \
+        .mean() \
+        .sort_values(by=['acc', 'log_loss'], ascending=[False, True]) \
+
+    print '\n\n', scores_mean
+
+    if cv_mean:
+        return scores_mean
+    else:
+        return scores
+
+
+classifiers = {
+    'knn': KNeighborsClassifier(
+        n_neighbors=6,
+        algorithm='ball_tree',
+        weights='distance',
+        metric='chebyshev'
+    ),
+    'knn_default': KNeighborsClassifier(),
+    'svc_rbf': SVC(
+        kernel='rbf',
+        C=50,
+        gamma=0.01,
+        tol=1e-3
+    ),
+    'svc_rbf_default': SVC(kernel='rbf'),
+    'svc_linear': LinearSVC(
+        C=9.33598911e+04,
+        loss='squared_hinge',
+        penalty='l2',
+        tol=9.76314258e-04
+    ),
+    'svc_linear_default': LinearSVC(),
+    'rf': RandomForestClassifier(
+        criterion='entropy',
+        max_depth=12,
+        min_samples_leaf=8,
+        min_samples_split=5
+    ),
+    'rf_default': RandomForestClassifier(),
+    'xgb': XGBClassifier(),
+    'adaboost': AdaBoostClassifier(),
+    'et': ExtraTreesClassifier(
+        criterion='entropy',
+        max_depth=25,
+        min_samples_leaf=5,
+        min_samples_split=5
+    ),
+    'et_default': ExtraTreesClassifier(),
+    'gbc_default': GradientBoostingClassifier(),
+    'lr_lbfgs': LogisticRegression(
+        # C=1000,
+        # tol=1e-3,
+        C=2.02739770e+04,  # particle swarm optimised
+        tol=6.65926091e-04,
+        solver='lbfgs'
+    ),
+    'lr_lbfgs_default': LogisticRegression(solver='lbfgs'),
+    'pa': PassiveAggressiveClassifier(
+        C=0.01,
+        fit_intercept=True,
+        loss='hinge'
+    ),
+    'pa_default': PassiveAggressiveClassifier(),
+    'gnb': GaussianNB(),
+    'lda': LinearDiscriminantAnalysis(),
+}
 
 
 if __name__ == '__main__':
@@ -80,120 +233,10 @@ if __name__ == '__main__':
     train = train.drop([target, 'image', 'filename'], axis=1)
     # train = train.drop([target, 'image'], axis=1)
 
-    # **************************************************************************
-    combined_features = Pipeline([
-        ('features', FeatureUnion([
-            ('scaler', StandardScaler()),
-            # ('poly', PolynomialFeatures(
-            #     degree=2,
-            #     interaction_only=True,
-            #     include_bias=False
-            # ))
-        ])),
-        # ('pca', Pipeline([
-        #     ('scaler', StandardScaler()),
-        #     ('pca', PCA()),
-        # ])),
-        # ('kpca', KernelPCA(n_components=10)),
-    ])
-
-    # **************************************************************************
-    classifiers = {
-        'knn': KNeighborsClassifier(
-            n_neighbors=6,
-            algorithm='ball_tree',
-            weights='distance',
-            metric='chebyshev'
-        ),
-        'knn_default': KNeighborsClassifier(),
-        'svc_rbf': SVC(
-            kernel='rbf',
-            C=50,
-            gamma=0.01,
-            tol=1e-3
-        ),
-        'svc_rbf_default': SVC(kernel='rbf'),
-        'svc_linear': LinearSVC(
-            C=9.33598911e+04,
-            loss='squared_hinge',
-            penalty='l2',
-            tol=9.76314258e-04
-        ),
-        'svc_linear_default': LinearSVC(),
-        'rf': RandomForestClassifier(
-            criterion='entropy',
-            max_depth=12,
-            min_samples_leaf=8,
-            min_samples_split=5
-        ),
-        'rf_default': RandomForestClassifier(),
-        'xgb': XGBClassifier(),
-        'adaboost': AdaBoostClassifier(),
-        'et': ExtraTreesClassifier(
-            criterion='entropy',
-            max_depth=25,
-            min_samples_leaf=5,
-            min_samples_split=5
-        ),
-        'et_default': ExtraTreesClassifier(),
-        'gbc': GradientBoostingClassifier(),
-        'lr_lbfgs': LogisticRegression(
-            # C=1000,
-            # tol=1e-3,
-            C=2.02739770e+04,  # particle swarm optimised
-            tol=6.65926091e-04,
-            solver='lbfgs'
-        ),
-        'lr_lbfgs_default': LogisticRegression(solver='lbfgs'),
-        'pa': PassiveAggressiveClassifier(
-            C=0.01,
-            fit_intercept=True,
-            loss='hinge'
-        ),
-        'pa_default': PassiveAggressiveClassifier(),
-        'gnb': GaussianNB(),
-        'lda': LinearDiscriminantAnalysis(),
-        'qda': QuadraticDiscriminantAnalysis(),
-    }
-
-    # **************************************************************************
-    scores = []
-    score_cols = [
-        'classifier', 'split', 'acc', 'precision', 'recall',
-        'f1', 'roc_auc'
-    ]
-
-    ss = ShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-
-    train = train.as_matrix()
-    for i, (train_idx, val_idx) in enumerate(ss.split(train, y_train_binary)):
-        X_train, X_val = train[train_idx], train[val_idx]
-        y_train, y_val = y_train_binary[train_idx], y_train_binary[val_idx]
-
-        for name, clf in classifiers.iteritems():
-
-            pipeline = Pipeline([
-                ('features', combined_features),
-                (name, clf)
-            ])
-            estimator = pipeline.fit(X_train, y_train)
-
-            y_pred = estimator.predict(X_val)
-
-            m, ps = scoring_metrics(y_val, y_pred, return_string=True)
-            ps += ' | {}_{}'.format(name, i)
-            print ps
-            scores.append([name, i] + m)
-
-    scores = pd.DataFrame(scores, columns=score_cols)
-    scores = scores.sort_values(
-        by=['acc', 'f1', 'roc_auc'],
-        ascending=False
-    ).reset_index(drop=True)
-
-    scores_mean = scores.ix[:, scores.columns != 'split'] \
-        .groupby(['classifier']) \
-        .mean() \
-        .sort_values(by=['acc', 'f1', 'roc_auc'], ascending=False) \
-
-    print '\n\n', scores_mean
+    cv_scores_mean = model_comparison(
+        classifiers=classifiers,
+        X=train.as_matrix(),
+        y=y_train_binary,
+        splitter=ShuffleSplit(n_splits=5, test_size=0.2, random_state=0),
+        cv_mean=True
+    )
