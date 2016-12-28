25ed0a39936ff4b7235781e41a9a07d945be145f
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-28 10:48:25 +0000

    Dataset generation refactored to class.

--- stegasawus/dataset.py
+++ stegasawus/dataset.py
@@ -1,153 +1,36 @@
+from stegasawus import lsb, seq
+
 import os
 import base64
 import cStringIO
 from PIL import Image
 
+from os import path, listdir
 from skimage import io
 
-from stegano import exifHeader
-from stegano import lsbset
-from stegano.lsbset import generators
 
-
-# ******************************************************************************
 def get_secret_message(filepath):
     """
-    Read text file.
-
-    Parameters
-    ----------
-    filepath : string
-        Input file.
-
-    Returns
-    -------
-    message : string
-        Contents of file.
-
+    Read text file, return message.
     """
     with open(filepath, 'rb') as f:
         message = f.read()
     return message
 
 
-def hide_message_jpg(secret_message, fp_cover, fp_stego):
-    """
-    Hide message in jpg file.
-
-    Parameters
-    ----------
-    secret_message : string
-        Message to hide.
-    fp_cover : string, filepath
-        Input image to hide message in.
-    fp_stego : string, filepath
-        Output image with hidden message embedded.
-
-    """
-    exifHeader.hide(fp_cover, fp_stego, secret_message=secret_message)
+def image_to_string(path_image):
+    with open(path_image, 'rb') as f:
+        return base64.b64encode(f.read())
 
 
-def hide_message_png(secret_message, fp_cover, fp_stego, generator):
-    """
-    Hide message in png file.
-
-    Parameters
-    ----------
-    secret_message : string
-        Message to hide.
-    fp_cover : string, filepath
-        Input image to hide message in.
-    fp_stego : string, filepath
-        Output image with hidden message embedded.
-    generator : string
-        Name of LSB embedding location generator
-        - identity
-        - eratosthenes
-        - eratosthenes_composite
-        - triangular_numbers
-        - syracuse
-
-    """
-    generator_set = {
-        'identity': generators.identity(),
-        'eratosthenes': generators.eratosthenes(),
-        'eratosthenes_composite': generators.eratosthenes_composite(),
-        # 'fibonacci': generators.fibonacci(),
-        # 'mersenne': generators.mersenne(),
-        'triangular_numbers': generators.OEIS_A000217(),
-        'syracuse': generators.syracuse(l=len(message)),
-    }
-
-    if generator not in generator_set:
-        generator = generators.identity()
-    else:
-        generator = generator_set[generator]
-
-    S = lsbset.hide(
-        fp_cover,
-        secret_message,
-        generator=generator
-    )
-    S.save(fp_stego)
+def string_to_image(image_string):
+    s = base64.b64decode(image_string)
+    s = cStringIO.StringIO(s)
+    return np.array(Image.open(s))
 
 
-def batch_hide_message(secret_message, path_images, path_output, file_type,
-                       generator=''):
-    """
-    Create steganographic images containing the hidden message using the Least
-    Significant Bit (LSB) algorithm.
-
-    Parameters
-    ----------
-    secret_message : string
-        Hidden message to embed in image.
-    path_images : string
-        Input image directory.
-    path_output : string
-        Output directroy for steganographic images.
-    file_type : string
-        'png' - embeds message in rgb pixels.
-        'jpg' - embeds message in discrete cosine coefficients.
-    generator : string, optional
-        Specify the embedding location generator.
-
-    Returns
-    -------
-    None
-        Steganographic images saved in path_output.
-
-    """
-
-    print 'encoding images...'
-    file_type = file_type.lower()
-
-    for i, filename in enumerate(os.listdir(path_images), start=1):
-        try:
-            cover = '{}{}'.format(path_images, filename)
-            stego = '{}{}'.format(path_output, filename)
-
-            if file_type == 'png':
-                hide_message_png(secret_message, cover, stego, generator)
-            elif file_type == 'jpg':
-                exifHeader.hide(cover, stego, secret_message=secret_message)
-            else:
-                print "file_type '%s' is not supported. \noptions = jpg, png."
-
-            print '{}: {}'.format(i, filename)
-        except IndexError as e:
-            print '{}: {} | IndexError: {}'.format(i, filename, e)
-        except Exception as e:
-            print '{}: {} | Exception: {}'.format(i, filename, e)
-
-    print 'image encoding complete.'
-
-
-def batch_hide_message_rnd_generator():
-    pass
-
-
-def crop_image(image, m, n, centre=True):
+def crop_image(image, dim, centre=True):
+    m, n = dim
     if centre:
         x, y, _ = image.shape
         x0 = int((x - m) / 2) - 1
@@ -159,86 +42,16 @@ def crop_image(image, m, n, centre=True):
         return image[0:m, 0:n]
 
 
-def read_string_image(filepath):
-    """
-    Reads and converts image string to RGB array.
-
-    Parameters
-    ----------
-    filepath : string, filepath
-        Text file to convert to image.
-
-    Returns
-    -------
-    I : numpy.ndarray
-        RGB image array.
-
-    """
-    with open(filepath, 'rb') as f:
-        s = base64.b64decode(f.read())
-        s = cStringIO.StringIO(s)
-        I = np.array(Image.open(s))
-    return I
-
-
-# TODO: generalise so any image can be used as benchmark or use relative paths
-def create_benchmark_image_message(m, n):
-    """
-    Creates benchmark image crops and converts to string for the given
-    dimensions. Used for creating standard message sizes for model training and
-    investigation.
-
-    Parameters
-    ----------
-    m : int
-        Row crop size.
-    n : int
-        Column crop size.
-
-    Returns
-    -------
-    Resized images of Lenna.png in data/messages directory.
-
-    """
-    path = '/home/rokkuran/workspace/stegasawus/'
-    path_msg = '{}data/messages/'.format(path)
-
-    I = io.imread(path_data + 'Lenna.png')
-    I = crop_image(I, m, n, centre=True)
-
-    output_filepath = path_msg + 'Lenna_{}x{}.png'.format(m, n)
-    io.imsave(arr=I, fname=output_filepath)
-
-    with open(output_filepath, 'rb') as fr:
-        s = base64.b64encode(fr.read())
-        filename = 'Lenna_{}x{}.txt'.format(m, n)
-        with open(path_msg + filename, 'wb') as fw:
-            fw.write(s)
-
-
-def crop_images(path_images, path_output, dimensions=(256, 256), centre=True):
+def crop_images(path_images, path_output, dimensions, centre=True):
     """
     Batch crop images from top left hand corner to dimensions specified. Skips
     images where dimensions are incompatible.
-
-    Parameters
-    ----------
-    path_images : string, directroy path
-        Directory with image set to crop.
-    path_output : string, directroy path
-        Directory for output of cropped images.
-    dimensions : array_like
-        Dimensions to crop image to: (n, m) array.
-    centre : bool
-        Whether to centre image crops or start from top left (0, 0).
-
     """
     print 'cropping images...'
-    m, n = dimensions
     for i, filename in enumerate(os.listdir(path_images)):
         try:
             image = io.imread('{}{}'.format(path_images, filename))
-            cropped = crop_image(image, m, n, centre=centre)
+            cropped = crop_image(image, dimensions, centre=centre)
             io.imsave(
                 fname='{}{}'.format(path_output, filename),
                 arr=cropped
@@ -250,7 +63,7 @@ def crop_images(path_images, path_output, dimensions=(256, 256), centre=True):
     print 'all images cropped and saved.'
 
 
-def batch_png_to_jpg(path_input, path_output):
+def batch_jpg_to_png(path_input, path_output):
     """
     Convert jpg images to png.
     """
@@ -267,6 +80,55 @@ def batch_png_to_jpg(path_input, path_output):
     print 'image conversion complete.'
 
 
-# ******************************************************************************
+class DatasetGenerator(object):
+    """
+    Generates dataset from
+    """
+    def __init__(self, path_images, path_output, seq_method):
+        super(DatasetGenerator, self).__init__()
+        self._path_images = path_images
+        self._path_output = path_output
+        self._seq_method = seq_method
+
+    def _read_embed_save(self, filename, message):
+        try:
+            path_cover = '{}{}'.format(self._path_images, filename)
+            path_stego = '{}{}'.format(self._path_output, filename)
+            I = io.imread(path_cover)
+            S = lsb.embed(I, message, self._seq_method)
+            io.imsave(arr=S, fname=path_stego)
+        except KeyError as e:
+            print '%s | message size greater than image capacity.' % filename
+
+    def batch_hide_message(self, message):
+        # TODO: cleanup error handling
+        for i, filename in enumerate(listdir(self._path_images), start=1):
+            file_type = filename.split('.')[-1]
+            if file_type == 'png':
+                self._read_embed_save(filename, message)
+                print '{}: {}'.format(i, filename)
+            else:
+                error = 'Image type not supported. Supported types: {png}'
+                raise Exception(error)
+
+        print 'image encoding complete.'
+
+
+def create_cropped_set(path_image, path_output, dims):
+    I = io.imread(path_image)
+    for m, n in dims.items():
+        pass
+
+
 if __name__ == '__main__':
-    pass
+    cdir = '/home/rokkuran/workspace/stegasawus/'
+    fp = path.join(cdir, 'data/messages/Lenna_64x64.png')
+
+    msg = image_to_string(fp)
+
+    path_images = '{}images/png/cover_test/'.format(cdir)
+    path_output = '{}images/png/lsb_test/'.format(cdir)
+
+    seq_method = seq.rand_darts(seed=77)
+    g = DatasetGenerator(path_images, path_output, seq_method)
+    g.batch_hide_message(msg)
