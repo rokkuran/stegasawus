937d40354d8b058b193f05add61ee9146ffda4aa
rokkkuran <lachlanbtaylor@gmail.com>
2016-12-28 10:48:58 +0000

    Sequence renaming.

--- stegasawus/seq.py
+++ stegasawus/seq.py
@@ -19,21 +19,21 @@ def skipy(y):
     return partial(_gen_skipy, y=y)
 
 
-def _gen_skip_rand(seed, max_skip, n, **kwargs):
+def _gen_rand_jump(seed, max_jump, n, **kwargs):
     i = 0
     x = 0
     np.random.seed(seed)
     while i <= n:
-        x += np.random.randint(1, max_skip)
+        x += np.random.randint(1, max_jump)
         yield x
         i += 1
 
 
-def skip_rand(seed, max_skip):
-    return partial(_gen_skip_rand, seed=seed, max_skip=max_skip)
+def rand_jump(seed, max_jump):
+    return partial(_gen_rand_jump, seed=seed, max_jump=max_jump)
 
 
-def _gen_skip_rand_restart(seed, max_skip, n, verbose=True, **kwargs):
+def _gen_rand_jump_circle(seed, max_jump, n, verbose=True, **kwargs):
     np.random.seed(seed)
     i = 0
     x = 0
@@ -42,20 +42,20 @@ def _gen_skip_rand_restart(seed, max_skip, n, verbose=True, **kwargs):
     while i <= n:
         if x >= n:
             # return to beginning of array once end reached
-            x = -1 + np.random.randint(1, max_skip)
+            x = -1 + np.random.randint(1, max_jump)
             n_resets += 1
             if verbose:
                 print 'x reset; n_resets = %d' % n_resets
         else:
-            x += np.random.randint(1, max_skip)
+            x += np.random.randint(1, max_jump)
             if x not in modified and x < n:
                 yield x
                 modified.append(x)
                 i += 1
 
 
-def skip_rand_restart(seed, max_skip):
-    return partial(_gen_skip_rand_restart, seed=seed, max_skip=max_skip)
+def rand_jump_circle(seed, max_jump):
+    return partial(_gen_rand_jump_circle, seed=seed, max_jump=max_jump)
 
 
 def _gen_rand_darts(seed, n):
